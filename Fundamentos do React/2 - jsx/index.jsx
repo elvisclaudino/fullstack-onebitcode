1be0cab4537680bb5574b8abbe19f8686f492976
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2024-01-31 13:51:48 -0300

    [react]: jsx

---
+++ Fundamentos do React/2 - jsx/index.jsx
@@ -0,0 +1,14 @@
+const element = <h1>Olá, mundo!</h1>;
+
+const anoterElement = (
+  <div>
+    <h1>Texto</h1>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+);
